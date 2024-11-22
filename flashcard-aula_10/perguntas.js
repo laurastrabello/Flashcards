e5802b6843cf13c8d72e49b873171b1b06d61f6b
laurastrabello <00001111599397sp@al.educacao.sp.gov.br>
2024-11-22 11:44:42 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'O que foi a ditadura militar no Brasil?',
+    'A Ditadura Militar foi um regime político comandado por membros das Forças Armadas. Seu período mais recente no Brasil vigorou por 21 anos, entre 1964 e 1985'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
